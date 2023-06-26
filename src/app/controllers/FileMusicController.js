dc8a3f519a697982a5b3278efe1c11afdce17cfc
Nguyen Dinh Thong <nguyenthong244466666@gmail.com>
2023-06-27 00:51:09 +0700

    setHeader

--- src/app/controllers/FileMusicController.js
+++ src/app/controllers/FileMusicController.js
@@ -1,12 +1,15 @@
-const FileMusic = require('../models/FileMusic');
+const FileMusic = require("../models/FileMusic");
 
 class FileMusicController {
-    // [GET] /fileMusic
-    get(req, res, next) {
-        FileMusic.findOne({musicId: req.params.musicId})
-            .then(fileFind => res.send(fileFind.data))
-            .catch(next)
-    }
+  // [GET] /fileMusic
+  get(req, res, next) {
+    FileMusic.findOne({ musicId: req.params.musicId })
+      .then((fileFind) => {
+        res.setHeader("Content-Type", fileFind.contentType);
+        res.send(fileFind.data);
+      })
+      .catch(next);
+  }
 }
 
-module.exports = new FileMusicController;
+module.exports = new FileMusicController();
